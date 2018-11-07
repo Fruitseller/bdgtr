d1b0c679613d40653c61eab138ed7f2380bd38bd
Peter Bronkalla <bronkalla@sipgate.de>
2018-11-07 17:07:10 +0100

    Add simple args parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        1 => {
+            println!("I am groot");
+        }
+        2 => {
+            println!("->  {}", args[1]);
+            if args[1] == "add" {
+                println!("add");
+            } else if args[1] == "expenses" {
+                println!("expenses");
+            }
+        }
+        _ => panic!("Too many arguments!"),
+    }
+    println!("args {:?}", args);
 }
